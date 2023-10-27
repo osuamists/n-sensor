d96e908fbbf9f455e405405c6b0ea304ef63e8b6
osuamists <134652319+osuamists@users.noreply.github.com>
2023-10-27 17:54:47 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,15 +1,34 @@
-// Simulando a captura de dados do sensor. Neste exemplo, usaremos um valor aleatório para a frequência.
-function getSensorData() {
-    return Math.floor(Math.random() * 100) + 50; // Valor aleatório entre 50 e 149 Hz.
+const powerButton = document.getElementById("power-button");
+const powerIcon = document.getElementById("power-icon");
+const ledElement = document.getElementById("led");
+const signalElement = document.getElementById("signal");
+
+let isLigado = false;
+
+function togglePower() {
+    isLigado = !isLigado;
+    if (isLigado) {
+        powerButton.classList.add("on");
+        powerIcon.textContent = "ON";
+        ledElement.textContent = "Ligado";
+    } else {
+        powerButton.classList.remove("on");
+        powerIcon.textContent = "OFF";
+        ledElement.textContent = "Desligado";
+    }
 }
 
-// Atualiza a frequência exibida na página a cada 3 segundos.
+// Simulando a captura de dados do sensor. Neste exemplo, usaremos um valor aleatório para o sinal do ar condicionado.
+function getSensorData() {
+    return Math.floor(Math.random() * 100) + 50; // Valor aleatório entre 50 e 149 em formato HEX.
+}
+
+// Atualiza o sinal exibido na página a cada 5 segundos.
 function updateSensorData() {
-    const frequencyElement = document.getElementById('frequency');
     setInterval(function() {
-        const frequency = getSensorData();
-        frequencyElement.textContent = frequency;
-    }, 3000);
+        const signal = getSensorData().toString(16).toUpperCase(); // Converte para formato HEX.
+        signalElement.textContent = signal;
+    }, 5000);
 }
 
 updateSensorData();
